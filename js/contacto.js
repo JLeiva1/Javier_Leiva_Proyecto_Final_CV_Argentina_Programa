d3fade6b76516ec1dd6cb7b58974b588a7f932eb
Javier <javier.leiva.01@gmail.com>
2023-03-20 21:03:50 -0300

    Se agregan validaciones al proyecto

---
+++ js/contacto.js
@@ -0,0 +1,163 @@
+//Variables
+let persona = 0;
+let email = false;
+let nameValidar = false;
+let apellidoValidar = false;
+let phoneValidar = false;
+let formEnviar = document.getElementById("fEnviarCont");
+//Se crean objetos
+let crearPersona = () => { persona = new Persona ({ nombre: userName, apellido: userLastName, email: userMail, phone: userPhone });}
+
+//Object
+class Persona{
+  constructor(literal){
+    this.nombre = literal.nombre;
+    this.apellido = literal.apellido;
+    this.email = literal.email;
+    this.phone = literal.phone;
+  }
+}
+
+function contacto () { 
+Swal.fire({
+    confirmButtonColor : "#ffb866",
+    showConfirmButton: false,
+    background: "transparent",
+
+    html: `          <div class="formularioMaps">
+    <form class="formulario">
+      <h2 class="formularioTitle">Ingresa tus datos y te contactamos</h2>
+      <div class="formularioContainer">
+        <div class="grupo">
+          <input type="text" id="name" class="formularioImput" placeholder=" ">
+          <label for="name" class="formularioLabel">Nombre:</label>
+          <span class="formularioLinea"></span>
+        </div>
+        <div class="grupo">
+          <input type="text" id="lastName" class="formularioImput" placeholder=" ">
+          <label for="lastName" class="formularioLabel">Apellido:</label>
+          <span class="formularioLinea"></span>
+        </div>
+        <div class="grupo">
+          <input type="text" id="mail" class="formularioImput" placeholder=" ">
+            <label for="mail" class="formularioLabel">Email:</label>
+            <span class="formularioLinea"></span>
+        </div>
+        <div class="grupo">
+        <input type="text" id="phone" class="formularioImput" placeholder=" ">
+          <label for="phone" class="formularioLabel">Tel:</label>
+          <span class="formularioLinea"></span>
+      </div>
+        <input type="button" class="formularioEnviar" value="Envia tus datos" id = "fEnviarCont" onclick="registrar();">
+      </div> 
+    </form>
+  </div>`,
+});
+}
+
+
+    //Alerta mail
+const alertMail = (leyenda,correo,icon) => {
+    Swal.fire({
+    title: `'Correo ${leyenda}'`,
+    text: `"La dirección de email ${correo} es ${leyenda}!."`,
+    icon: `${icon}`,
+    showConfirmButton: true,
+  })
+    }
+
+const datosError = (mensaje) => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        showConfirmButton: true,
+        text: `${mensaje}`,
+      })
+}
+
+const registroExitoso = () => {
+  Swal.fire({
+    toast: true,
+    position: 'top-end',
+    icon: 'success',
+    title: 'Datos enviados con exito',
+    showConfirmButton: true,
+    timer: 1500,
+    timerProgressBar: true
+  })
+  console.log("OK")
+}
+
+    //Validar correo
+const validarEmail = (valor) => {
+    if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
+     email = true;
+    } else {
+       alertMail("invalido", valor, 'error');
+     email = false;
+    }
+   }
+
+   const validarDatos = (dato,dato2) => {
+    if (dato2 == "1"){
+    if(new RegExp('^[A-Z]+$', 'i').test(dato)){
+      nameValidar = true;
+     } else {
+        datosError("El nombre solo puede contener caracteres alfabéticos");
+      nameValidar = false;
+     }
+    }
+    if (dato2 == "2"){
+      if(new RegExp('^[A-Z]+$', 'i').test(dato)){
+        apellidoValidar = true;
+       } else {
+          datosError("El apellido solo puede contener caracteres alfabéticos");
+        apellidoValidar = false;
+       }
+      }
+    }
+
+     const validarPhone = (datoPhone) => {
+       if(new RegExp('^[0-9]+$', 'i').test(datoPhone)){
+         phoneValidar = true;
+        } else {
+           datosError("El campo teléfono solo puede contener caracteres numericos");
+         phoneValidar = false;
+        }
+       }
+
+   //Registrar datos
+function registrar(){
+     userName = document.getElementById("name").value;
+     userLastName = document.getElementById("lastName").value;
+     userMail = document.getElementById("mail").value;
+     userPhone = document.getElementById("phone").value;
+
+    if((userName == "") || (userLastName == "") || (userMail == "") || (userPhone == "")){
+        datosError("Contiene campos sin completar");
+    }
+
+    else if (userName && userLastName && userMail && userPhone != " "){
+        validarDatos(userName,"1");
+        validarDatos(userLastName,"2");
+        validarPhone(userPhone);
+        validarEmail(userMail);
+        if (email == true && nameValidar == true && apellidoValidar == true && phoneValidar == true){
+        crearPersona();
+        const enJSON = JSON.stringify(persona);
+        localStorage.setItem(`Correo:${userMail}`, enJSON)
+        registroExitoso();
+        }
+        else {
+        console.log("Uno o más datos pueden ser incorrectos")
+        }
+    }
+    else {
+      datosError("Contiene campos sin completar o caracteres invalidos")
+      }
+}
+
+//Ejecutar codigo
+ formEnviar.onclick = () => {
+    registrar();
+ }
--- js/modoOscuro.js
+++ js/modoOscuro.js
@@ -2,23 +2,25 @@
 let modo=document.getElementById("modoVis")
 
 modo.addEventListener ("click", function(){
-    if (modo.className.includes("fa-sun")){
-        modo.className= "fa-moon";
+    if (modo.className.includes("fa-suna")){
+        modo.className= "fa-moona";
         modo.title="Modo oscuro activo";
         dayNightColor('--color-1','#D6D3E7');
         dayNightColor('--color-2','#5A5972');
         dayNightColor('--color-3','rgb(255, 182, 47)');
         dayNightColor('--color-pagina','#302D3E');
+        dayNightColor('--color-titulo','darkgray');
         dayNightColor('--color-texto','darkgray');
         dayNight("Icons/night.png");
         
     } else {
-        modo.className="fa-sun";
+        modo.className="fa-suna";
         modo.title="Modo claro activo";
-        dayNightColor('--color-1', '#A9E7F8');
-        dayNightColor('--color-2', '#c2a0a0');
+        dayNightColor('--color-1', 'rgb(253, 131, 93)');
+        dayNightColor('--color-2', 'rgb(214, 106, 106)');
         dayNightColor('--color-3', '#d86060');
-        dayNightColor('--color-pagina', 'white');
+        dayNightColor('--color-pagina', 'rgb(255, 239, 234)');
+        dayNightColor('--color-titulo','rgb(146, 0, 0)');
         dayNightColor('--color-texto', 'black');
         dayNight("Icons/sun.png");
     }
